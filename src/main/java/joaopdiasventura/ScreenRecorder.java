7880fdfb77b5e7ffe7db19a249d49f1e3461db06
João Paulo Dias <joaopdiasventura@gmail.com>
2024-05-14 14:31:44 -0300

    fix: tirando a tela vermelha

--- src/main/java/joaopdiasventura/ScreenRecorder.java
+++ src/main/java/joaopdiasventura/ScreenRecorder.java
@@ -1,10 +1,13 @@
-package org.example;
+package joaopdiasventura;
 
 import java.awt.AWTException;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorConvertOp;
+import java.io.File;
+import java.util.Objects;
 import java.util.Scanner;
 
 import org.bytedeco.ffmpeg.global.avcodec;
@@ -15,39 +18,62 @@ public class ScreenRecorder {
     private final Rectangle screenRect;
     private final Robot robot;
     private final FFmpegFrameRecorder recorder;
+    private volatile boolean ended;
 
     public ScreenRecorder(String videoFileName) throws AWTException {
         this.screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
         this.robot = new Robot();
-        this.recorder = new FFmpegFrameRecorder(videoFileName, screenRect.width, screenRect.height);
+
+        File videoDirectory = new File("videos");
+        if (!videoDirectory.exists()) {
+            videoDirectory.mkdirs();
+        }
+
+        this.recorder = new FFmpegFrameRecorder(videoDirectory + File.separator + videoFileName, screenRect.width, screenRect.height);
         this.recorder.setFormat("mp4");
-        this.recorder.setVideoCodec(27);
-        this.recorder.setVideoQuality(1);
+        this.recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
+        this.recorder.setVideoQuality(0);
         this.recorder.setFrameRate(30);
+        this.recorder.setVideoBitrate(4000 * 1024);
+        this.recorder.setGopSize(60);
     }
 
     public void start() throws FrameRecorder.Exception, InterruptedException {
         System.out.println("Starting Record...");
+        ended = false;
         recorder.start();
-        long startTime = System.currentTimeMillis();
-        while (true) {
-            long now = System.currentTimeMillis();
-            if (now - startTime > 5000) {
-                break;
+        new Thread(() -> {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Press 'q' to stop the recording");
+            while (!ended) {
+                String input = sc.nextLine();
+                if (Objects.equals(input, "q")) {
+                    stop();
+                }
             }
+            sc.close();
+        }).start();
+        while (!ended) {
             BufferedImage screenCapture = robot.createScreenCapture(screenRect);
             Frame frame = convertImageToFrame(screenCapture);
-            recorder.record(frame,1);
-            Thread.sleep(33);
+            recorder.record(frame);
+            Thread.sleep(60);
         }
         recorder.stop();
         recorder.release();
         System.out.println("Recording Ended");
     }
 
+    public void stop() {
+        this.ended = true;
+    }
+
     private Frame convertImageToFrame(BufferedImage bufferedImage) {
+        BufferedImage convertedImage = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+        ColorConvertOp op = new ColorConvertOp(null);
+        op.filter(bufferedImage, convertedImage);
         Java2DFrameConverter converter = new Java2DFrameConverter();
-        return converter.getFrame(bufferedImage);
+        return converter.getFrame(convertedImage);
     }
 
     public static void main(String[] args) throws AWTException, FrameRecorder.Exception, InterruptedException {
